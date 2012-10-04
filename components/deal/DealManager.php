e8e59680142cbe3e31001464cea2d2f2c1b936ee
tomer harry <tomer.harry@gmail.com>
2012-10-04 15:20:01 +0200

    added Include Component Class

--- components/deal/DealManager.php
+++ components/deal/DealManager.php
@@ -16,31 +16,11 @@ class DealManager implements IDealManager
 
     private function showLeaderDeal($leaderDeal)
     {
-        includeModel("slogan");
-        includeModel("sliderPhrase");
-        includeModel("leaderReward");
-/*
-//$temp = new slogan('a','b');
-        $slogan = slogan::getSlogan('DefCamp');
-
-//temp vad todo: remove it
-        $slogan->firstLine = "You got the power!!!";
-        $slogan->secondLine = "Create an Awesome Deal";
-
-        $temp1 = new sliderPhrase('a', 'b');
-        $sliderPhrase = $temp1->getSliderPhrase('DefCamp');
-
-        $temp2 = new leaderReward('1', 'b', 'a', 'd', 10);
-        $leaderReward = $temp2->getleaderReward('DefCamp');
-
-        $GLOBALS["page_title"] = "CatBee Landing Page";
-        $GLOBALS["title"] = $slogan->firstLine;
-        $GLOBALS["subtitle"] = $slogan->secondLine;
-*/
         $GLOBALS["leaderDeal"] = $leaderDeal;
 
-        catbeeLayoutComp($layout, "inputforms/landing", $leaderDeal);
-        catbeeLayoutComp($layout, "inputforms/sliderOptions", $leaderDeal);
+        catbeeLayoutComp($layout, "landing", $leaderDeal);
+        catbeeLayoutComp($layout, "share", $leaderDeal);
+        catbeeLayoutComp($layout, "sliderOptions", $leaderDeal);
 
         catbeeLayout($layout, 'landing');
 
@@ -57,23 +37,18 @@ class DealManager implements IDealManager
         $leaderDeal->order = $order;
         $leaderDeal->status = LeaderDeal::$STATUS_PENDING;
         $leaderDeal->selectedLandingReward = $landing->landingRewards[0]->leaderReward->value;
-
         $this->dealDao->insertDeal($leaderDeal);
-
         return $leaderDeal;
     }
 
 
     public function pushDeal($order)
     {
+
         $campaign = $this->campaignManager->chooseCampaign($order);
-
         $leaderLanding = $this->campaignManager->chooseLeaderLanding($campaign, $order);
-
         $leaderDeal = $this->createPendingDeal($leaderLanding, $order);
-
         $this->showLeaderDeal($leaderDeal);
-
         return $leaderDeal;
     }
 }
