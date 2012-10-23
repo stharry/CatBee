7e2bf84ed542d583192c2869cf53e34387b191fe
spidernah <spidernah@gmail.com>
2012-10-23 22:37:25 +0200

    facebook goes live

--- adapters/demo/api/index.php
+++ adapters/demo/api/index.php
@@ -1,8 +1,26 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: vadim
- * Date: 08/10/12
- * Time: 19:35
- * To change this template use File | Settings | File Templates.
- */
+
+include_once($_SERVER[ 'DOCUMENT_ROOT' ] . "/CatBee/components/rest/RestUtils.php");
+include_once($_SERVER[ 'DOCUMENT_ROOT' ] . "/CatBee/components/rest/RestLogger.php");
+include_once('../components/CatBeeFacade.php');
+
+RestLogger::log("Share api call started");
+
+$return_obj = RestUtils::processRequest() or die ("Cannot parse post data in share request");
+$shareProps = $return_obj->getRequestVars() or die("Unknown share format");
+
+if (!isset($shareProps['act'])) die("Undefined adapter action");
+
+$facade = new CatBeeFacade();
+
+switch (strtolower($shareProps['act']))
+{
+    case 'welcome':
+    {
+        $deal = $facade->GetCatBeeDealByNo($shareProps['pdl']);
+
+        $currentCustomer = $facade->GetCatBeeLoggedinCustomer($shareProps['shr']);
+
+        //???
+    }
+}
