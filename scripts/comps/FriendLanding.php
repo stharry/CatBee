8dfd41761163c126639310f075456b3b584f911f
spidernah <spidernah@gmail.com>
2012-10-24 22:08:02 +0200

    facebook goes live

--- scripts/comps/FriendLanding.php
+++
@@ -1,7 +0,0 @@
-<div class="deal-text">
-    Tim <?=$p->friendMessage?><br />
-    <p> <?=$p->slogan?></p>
-    <?=$p->rewardMessage1?><br />
-    <?=$p->rewardMessage2?><br />
-    <div class="clearfix"><a href="#" class="go-btn">Go</a></div>
-</div>
