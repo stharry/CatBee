f010960292a0a3757576174c89209bd4c459e153
tomer harry <tomer.harry@gmail.com>
2012-11-07 15:27:07 +0200

    FriendLanding Page Fix

--- scripts/comps/friendLanding.php
+++ scripts/comps/friendLanding.php
@@ -1,7 +1,7 @@
 <div class="deal-text">
-    Tim <?=$p->landing->friendMessage?><br />
-    <p> <?=$p->landing->slogan?></p>
-    <?=$p->landing->rewardMessage1?><br />
-    <?=$p->landing->rewardMessage2?><br />
+    Tim <?=$p[0]->friendMessage?><br />
+    <p> <?=$p[0]->slogan?></p>
+    <?=$p[0]->rewardMessage1?><br />
+    <?=$p[0]->rewardMessage2?><br />
     <div class="clearfix"><a href="#" class="go-btn">Go</a></div>
 </div>
