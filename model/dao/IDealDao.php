02c1a4191aafb9858f93c02fa159651d617fadef
spidernah <spidernah@gmail.com>
2012-11-11 09:10:36 +0200

    facebook goes live

--- model/dao/IDealDao.php
+++ model/dao/IDealDao.php
@@ -4,6 +4,8 @@ interface IDealDao
 {
     public function getDealById($id);
 
+    public function getParentDealByOrderId($id);
+
     public function getDealByOrder($order);
 
     public function insertDeal($deal);
