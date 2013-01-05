9368c0ef6ac9b4226cfdc2b70036f57ba2e60751
tomer harry <tomer.harry@gmail.com>
2013-01-06 00:25:03 +0200

    Full CycleTest

--- tests/json/friendDeal.php
+++ tests/json/friendDeal.php
@@ -5,5 +5,13 @@ IncludeComponent('rest', 'RestUtils');
 
 $friendDealTemplate = json_decode(file_get_contents("res/friendDeal.json"));
 
+//I need To retrieve the ActiveSHareID of the Default and build the Friend Deal Json like in GoWelcome
+
 $restUtils = new RestUtils();
-$restUtils->SendPostRequest("deal", "", $friendDealTemplate);
+
+$temp = $restUtils->SendPostRequest("deal", "", $friendDealTemplate);
+
+if($temp !=null)
+    echo "5";
+
+
