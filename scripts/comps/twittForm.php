5ffe6e6b4dccf505c259fbc94258ae8e41c42142
spidernah <comspidernah@gmail>
2013-01-19 14:34:01 +0200

    Zoran second zip

--- scripts/comps/twittForm.php
+++ scripts/comps/twittForm.php
@@ -1,1 +1,6 @@
 <div id="tbox" style="display: none;"></div>
+<div id="tbox_bottom" style="display: none;"></div>
+<?php /* <input type="hidden" id="twitter_link" />
+<script type="text/javascript">
+	$('#twitter_link').val(TribZi.deal.twitContext.link);
+</script> */ ?>
