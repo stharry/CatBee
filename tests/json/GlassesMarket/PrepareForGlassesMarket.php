f2181bca426913cc275bd2adf417982285b6ae5d
tomer harry <tomer.harry@gmail.com>
2013-01-22 13:02:06 +0200

    GM Test

--- tests/json/GlassesMarket/PrepareForGlassesMarket.php
+++ tests/json/GlassesMarket/PrepareForGlassesMarket.php
@@ -13,12 +13,18 @@ $restUtils->SendPostRequest("store", "", $Store);
 $campaign = json_decode(file_get_contents("pushGMCampaign.json"));
 $restUtils->SendPostRequest("campaign", "", $campaign);
 
-//includeTest('addStoreShareTemplates.php');
-//includeTest('pushDeal.php');
-//includeTest('shareStoreShareTemplate.php');
-//includeTest('friendDeal.php');
-//includeTest('pushReferredDeal.php');
+$dir = $GLOBALS[ "dirBase" ] . '/tests/json/GlassesMarket/GMadd*ShareTemplate.json';
 
+foreach (glob($dir) as $ff)
+{
+    echo $ff;
+    $shareTemplate = json_decode(file_get_contents($ff));
+
+    $restUtils = new RestUtils();
+    $restUtils->SendPostRequest("share", "", $shareTemplate);
+
+    echo "Add ".basename($ff)." - OK </p>";
+}
 
 function includeTest($testName)
 {
