0b0c7b3ee275db16bcc39607da9d08a42c36a48a
tomer harry <tomer.harry@gmail.com>
2013-01-23 15:18:26 +0200

    email Validation

--- public/res/js/email.js
+++ public/res/js/email.js
@@ -83,7 +83,7 @@ function validateEmail(email) {
     for (i = 0; i < emails.length; i++) {
         var m = emails[i].trim();
         var reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!reg.test(emails[i])) {
+        if (!reg.test(m)) {
             return false;
         }
     }
