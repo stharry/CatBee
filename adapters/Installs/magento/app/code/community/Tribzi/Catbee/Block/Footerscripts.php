9c9380373fe4e50b8326363164c8e761dfa1e0ce
spidernah <comspidernah@gmail>
2013-02-02 22:12:57 +0200

    Friend Widget Design

--- adapters/Installs/magento/app/code/community/Tribzi/Catbee/Block/Footerscripts.php
+++ adapters/Installs/magento/app/code/community/Tribzi/Catbee/Block/Footerscripts.php
@@ -8,8 +8,8 @@ class Tribzi_Catbee_Block_Footerscripts extends Mage_Core_Block_Template
   protected function _beforeToHtml() {
     $uriPrefix = !empty($_SERVER['HTTPS']) ? 'https://api.tribzi.com/' : 'http://api.tribzi.com/';
     //TODO: make this configurable via layout.xml
-    $srcs[] = $uriPrefix . 'CatBee/adapters/Installs/catbeewelcome.js';
-    $srcs[] = $uriPrefix . 'CatBee/adapters/Installs/catbeeframe.js';
+      $srcs[] = $uriPrefix . 'CatBee/adapters/Installs/catbeeframe.js';
+      $srcs[] = $uriPrefix . 'CatBee/adapters/Installs/catbeecart.js?cfid=coupon_code';
     $this->setScripts($srcs);
   }
 }
--- adapters/Installs/magento/app/code/community/Tribzi/Catbee/Block/Popup.php
+++ adapters/Installs/magento/app/code/community/Tribzi/Catbee/Block/Popup.php
@@ -9,7 +9,7 @@ class Tribzi_Catbee_Block_Popup extends Mage_Core_Block_Template
     if ($params = Mage::getSingleton('checkout/session')->getData('catbee')) {
       $uriPrefix = !empty($_SERVER['HTTPS']) ? 'https://api.tribzi.com/' : 'http://api.tribzi.com/';
       $paramsStr = 'id=' . $params['id'] . '&to=' . $params['subTotal'] . '&cn=' . $params['name'] . '&ce=' . $params['email'];
-      $src = $uriPrefix .  'CatBee/adapters/Installs/Embedded_Magento.js?reload&' . $paramsStr;
+      $src = $uriPrefix .  'CatBee/adapters/Installs/catbeepostpurchase.js?reload&' . $paramsStr;
       $this->setScript($src);
       Mage::getSingleton('checkout/session')->unsetData('catbee');
     }
