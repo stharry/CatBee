25d1923c53baf83b6fd9b28cf374e903db9ee37a
spidernah <spidernah@gmail.com>
2013-02-04 13:48:37 +0200

    GM

--- adapters/Installs/magento/app/code/community/Tribzi/Catbee/Block/Popup.php
+++ adapters/Installs/magento/app/code/community/Tribzi/Catbee/Block/Popup.php
@@ -8,9 +8,8 @@ class Tribzi_Catbee_Block_Popup extends Mage_Core_Block_Template
   protected function _beforeToHtml() {
     if ($params = Mage::getSingleton('checkout/session')->getData('catbee')) {
       $uriPrefix = !empty($_SERVER['HTTPS']) ? 'https://api.tribzi.com/' : 'http://api.tribzi.com/';
-      $paramsStr = 'id=' . $params['id'] . '&to=' . $params['subTotal'] . '&cn=' . $params['name']
-          . '&ce=' . $params['email'].'&shop=2&adpt=54c5e6c3-6349-11e2-a702-0008cae720a7';
-      $src = $uriPrefix .  'CatBee/adapters/Installs/catbeepostpurchase.js?reload&' . $paramsStr;
+      $paramsStr = http_build_query($params);
+      $src = $uriPrefix .  'CatBee/adapters/Installs/catbeepostpurchase.js?' . $paramsStr;
       $this->setScript($src);
       Mage::getSingleton('checkout/session')->unsetData('catbee');
     }
--- adapters/Installs/magento/app/code/community/Tribzi/Catbee/Model/Cart/Observer.php
+++ adapters/Installs/magento/app/code/community/Tribzi/Catbee/Model/Cart/Observer.php
@@ -5,21 +5,30 @@
  */
 class Tribzi_Catbee_Model_Cart_Observer
 {
-  public function addCatbee(Varien_Event_Observer $observer)
-  {
-      $orderIds = $observer->getEvent()->getOrderIds();
-      if (empty($orderIds) || !is_array($orderIds)) {
-          return;
-      }
-      $order = Mage::getModel('sales/order')->load(reset($orderIds));
-      $catBeeArray = array(
-        'id' => $order->getId(),
-        'email' => $order->getCustomerEmail(),
-        'name' => $order->getCustomerName(),
-        'subTotal' => $order->getGrandTotal() - $order->getShippingAmount(),
-      );
-      Mage::getSingleton('checkout/session')->setData('catbee', $catBeeArray);
-      Mage::log($catBeeArray);
-  }
+    public function addCatbee(Varien_Event_Observer $observer)
+    {
+        $orderIds = $observer->getEvent()->getOrderIds();
+        if (empty($orderIds) || !is_array($orderIds))
+        {
+            return;
+        }
+        $order       = Mage::getModel('sales/order')->load(reset($orderIds));
+        $data        = $order->getData();
+        $items       = $order->getAllItems();
+        $fistItem    = reset($items);
+        $prod        = Mage::getModel('catalog/product')->load($fistItem->getProductId());
+        $catBeeArray = array(
+            'id'    => $order->getId(),
+            'ce'    => $order->getCustomerEmail(),
+            'cn'    => $order->getCustomerName(),
+            'ot'    => $data[ 'subtotal' ],
+            'icnt'  => 1, //count($items),
+            'i0upc' => $prod->getSku(),
+            'i0url' => Mage::getModel('catalog/product_media_config')->getMediaUrl($prod->getImage()),
+            'shop'  => 2, //Mage::getStoreConfig('tribzi/tribziGlobal/shopId'),
+            'adpt'  => '54c5e6c3-6349-11e2-a702-0008cae720a7' //Mage::getStoreConfig('tribzi/tribziGlobal/adapterId'),
+        );
+        Mage::getSingleton('checkout/session')->setData('catbee', $catBeeArray);
+    }
 }
 
