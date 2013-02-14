4a3e25000cd7dab8892b8e264f82f840d0b1bdba
spidernah <comspidernah@gmail>
2013-02-14 11:33:17 +0200

    bug fixes

--- adapters/Installs/shops/2/friendLanding.js
+++ adapters/Installs/shops/2/friendLanding.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
 
     $('.box').mutate('height width',function (element,info){
-        TribZi.resizeFrame(0, 0, '.box-wrapper');
+        TribZi.resizeFrame(2, 2, '.box-wrapper');
     });
 
     var imageSrc = "http://media-cache-lt0.pinterest.com/550/b9/eb/78/b9eb786ad849a05497051c97d0defeab.jpg";
 
-    $("#product-img").attr("src", imageSrc);
+    setTimeout(function(){
+        $("#product-img").attr("src", imageSrc);
+    }, 200);
+
 
     $('#go-btn').click(function(){
 
         TribZi.saveCoupon().closeFrame();
     });
 
+
+    TribZi.resizeFrame(2, 2, '.box-wrapper');
 
 });
