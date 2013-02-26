51e9b4508368cfead335322a9fd70f33a0a73075
Chaim Kutnicki <chaimkut@gmail.com>
2013-02-26 11:33:50 +0200

    Correct mismatch of Pinterest ID names

--- scripts/comps/pinterestForm.php
+++ scripts/comps/pinterestForm.php
@@ -1,4 +1,4 @@
-<div class="pinterest-form rounded-bottom" id="pinterestBox">
+<div class="pinterest-form rounded-bottom" id="pinterestForm">
     <p>Use this message or, better yet, write your own.</p>
     <div class="pinterest-form-fields">
         <textarea id="pinterest-message" title="Tell your Pinterest followers about your new purchase"></textarea>
