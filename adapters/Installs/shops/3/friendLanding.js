f582dadc2caeafd9191f3094355c71d06bc6bbff
spidernah <comspidernah@gmail>
2013-03-05 14:33:36 +0200

    CTT

--- adapters/Installs/shops/3/friendLanding.js
+++ adapters/Installs/shops/3/friendLanding.js
@@ -4,11 +4,15 @@ $(document).ready(function () {
         TribZi.resizeFrame(2, 2, '.box-wrapper');
     });
 
+//    $('#support-email').attr("href", "mailto:service.charmstotreasure@gmail.com");
+//    $('#support-email').text("service.charmstotreasure@gmail.com");
+
     var imageSrc = "http://sphotos-b.ak.fbcdn.net/hphotos-ak-snc6/180251_10150132162005056_6394899_n.jpg";
 
-    setTimeout(function(){
+    setTimeout(function()
+    {
         $("#product-img").attr("src", imageSrc);
-    }, 200);
+    },        200);
 
 
     $('#go-btn').click(function(){
