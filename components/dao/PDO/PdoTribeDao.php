416ae36e17afe3ff1b0ab8f85ee00558a83a531b
tomer harry <tomer.harry@gmail.com>
2013-03-17 11:29:03 +0200

    Tribe

--- components/dao/PDO/PdoTribeDao.php
+++ components/dao/PDO/PdoTribeDao.php
@@ -9,11 +9,11 @@ class PdoTribeDao
 
          try
          {
-             $selectClause = " select TR.id as TribeID,TR.tribeName,Cu.email,Cu.firstName,Cu.lastName from tribes as TR inner join tribecustomers as TC";
+             $selectClause = " select TR.id as TribeID,TR.tribeName,Cu.email,Cu.firstName,Cu.lastName from tribes as TR inner join tribeCustomers as TC";
              $selectClause = $selectClause." on TR.id=TC.TribeId";
              $selectClause = $selectClause. " inner join customers Cu on TC.CustomerID=Cu.id";
              $selectClause = $selectClause." where TC.TribeId = ";
-             $selectClause = $selectClause."(select TribeId from tribecustomers inner join customers on tribecustomers.CustomerID=customers.id where email='";
+             $selectClause = $selectClause."(select TribeId from tribeCustomers inner join customers on tribeCustomers.CustomerID=customers.id where email='";
             $selectClause = $selectClause. $tribeFilter->customers->email;
              $selectClause = $selectClause."')";
              $rows = DbManager::selectValues($selectClause, null);
