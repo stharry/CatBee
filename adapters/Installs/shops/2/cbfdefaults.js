2ba20d09396ba2d41c69985e039649289744130b
spidernah <comspidernah@gmail>
2013-03-18 10:55:46 +0200

    Change the DealPDO to support Filter

---
+++ adapters/Installs/shops/2/cbfdefaults.js
@@ -0,0 +1,20 @@
+cbfSettings.redirectTo = 'https://apid.tribzi.com/CatBee/';
+
+cbfSettings.widgets.ppw = {
+    gui:{
+        appendTo   :'cbfRoot',
+        setBefore  :null,
+        closeButton:true
+    },
+    sites:{
+        run:false
+    }};
+
+cbfSettings.widgets.cpn = {
+    gui:{
+        appendTo:'coupon_code'
+    },
+    sites:{
+        run:true
+    }
+};
